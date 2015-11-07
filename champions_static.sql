3d225497bd2ae867a0336b2f68ff2fb0ccae9415
Ilya Danilkin <nezorflame@gmail.com>
2015-11-08 00:02:11 +0300

    Static champions data

---
+++ champions_static.sql
@@ -0,0 +1,21 @@
+﻿-- Table: lol_api.champions_static
+
+-- DROP TABLE lol_api.champions_static;
+
+CREATE TABLE lol_api.champions_static
+(
+  id integer NOT NULL,
+  blurb text,
+  image bytea,
+  "allytipsID" integer,
+  "enemytipsID" integer,
+  info text,
+  CONSTRAINT "cstaticID" PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE lol_api.champions_static
+  OWNER TO postgres;
+COMMENT ON TABLE lol_api.champions_static
+  IS 'lol-static-data-v1.2 [BR, EUNE, EUW, KR, LAN, LAS, NA, OCE, PBE, RU, TR]';
